77fd536f8376d209bd933922caee2538352d23b9
rahman sanuth <sanuthrahman@gmail.com>
2022-08-09 20:21:14 +0100

    fixed version bug

--- config.py
+++ config.py
@@ -10,6 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://jvoppuakifcjlm:fc1ba1a0767f76e61c37983b5418a0498e9d4cdf9a39a854c59c4eef5b57ba40@ec2-34-234-240-121.compute-1.amazonaws.com:5432/dd7ecs14pelnip
-'
+SQLALCHEMY_DATABASE_URI = 'postgresql://jvoppuakifcjlm:fc1ba1a0767f76e61c37983b5418a0498e9d4cdf9a39a854c59c4eef5b57ba40@ec2-34-234-240-121.compute-1.amazonaws.com:5432/dd7ecs14pelnip'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
